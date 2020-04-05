d14ee5ab78467ced0245a3354451d958605db441
kasettakorn <kasettakorn_techno@hotmail.com>
2020-04-05 14:04:03 +0700

    Build commit

---
+++ build/precache-manifest.a90f95f724d42b5527ffee9861b204f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a925ae5b14759211573350b1bb8bc5b4",
+    "url": "/kasettakorn/objectDetection_tfjs.git/index.html"
+  },
+  {
+    "revision": "2871f4596d11d60a8d26",
+    "url": "/kasettakorn/objectDetection_tfjs.git/static/css/2.ad8fe80d.chunk.css"
+  },
+  {
+    "revision": "3241e313b7aa22ad850e",
+    "url": "/kasettakorn/objectDetection_tfjs.git/static/css/main.2be08301.chunk.css"
+  },
+  {
+    "revision": "2871f4596d11d60a8d26",
+    "url": "/kasettakorn/objectDetection_tfjs.git/static/js/2.7185c909.chunk.js"
+  },
+  {
+    "revision": "921adb173c5d83dcf71d6a9bc3cbe81a",
+    "url": "/kasettakorn/objectDetection_tfjs.git/static/js/2.7185c909.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3241e313b7aa22ad850e",
+    "url": "/kasettakorn/objectDetection_tfjs.git/static/js/main.acfb5ab8.chunk.js"
+  },
+  {
+    "revision": "ce6fba36498d816318e5",
+    "url": "/kasettakorn/objectDetection_tfjs.git/static/js/runtime-main.f916e097.js"
+  }
+]);
